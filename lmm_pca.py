043e310a31db89f184c93ea5b198afc45f13296b
Hao-Ting Wang <htwangtw@gmail.com>
2020-07-16 20:17:20 +0100

    refine the linear mixed model

--- lmm_pca.py
+++ lmm_pca.py
@@ -1,94 +1,197 @@
+#%% load libraries
 import pandas as pd 
 import numpy as np 
+
 from sklearn.decomposition import PCA
+
 from scipy.stats import zscore
+from scipy import linalg
+
+import seaborn as sns 
 import matplotlib.pyplot as plt
+
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-from util import varimax
+from util import varimax, correct_scale
 
-data_path = "data/CS_MWQ_LabOnlineThoughtProbesScores_rescaled.csv"
-data = pd.read_csv(data_path)
+#%% load data and basic clean up
 
-print(np.unique(data.RIDNO).shape)
+es_path = "data/CS_MWQ_LabOnlineThoughtProbesScores_rescaled.csv"
+task_path = "data/CS_Tasks_withEFF.csv"
+task_labels = ["RNO", "MWQ_WM.ACC", "MWQ_WM.RT", "MWQ_CRT.ACC", "MWQ_CRT.RT"]
+new_labels = ["RIDNO", "oneBackAcc", "oneBackRT", "zeroBackAcc", "zeroBackRT"]
+data = pd.read_csv(es_path)
+task = pd.read_csv(task_path)[task_labels]
+task.columns = new_labels
 
 # drop rows with no data
 data = data.dropna()
-print(np.unique(data.RIDNO).shape)
+data["group"] = 0  # nul model
+
+# add behavioural task performace
+data["Acc"] = 0
+data["RT"] = 0
+data["nBack"] = data["nBack"].astype(int)
+
+for id in np.unique(data.RIDNO).tolist():
+    cur_task = task[task['RIDNO'].str.match(id)]
+    for b, con in enumerate(["zero", "one"]):
+        id_idx = data.query(f"nBack=={b} & RIDNO=='{id}'").index
+        for t in ["Acc", "RT"]:
+            data.loc[id_idx, t] = cur_task[f"{con}Back{t}"].values
+
+# use the first cohort
+data = data[data.IDNO < 500]
+
+# normalise the scale to 0 -1 based on scale range used per individual
+data = correct_scale(data)
+
+#%% naive PCA
 
 # SPSS PCA was performed on correlation matrix 
 # so we z-score the input data
-clean_data = zscore(data.iloc[:, 4:].values)
+X = data.loc[:, 'MWQ_Focus':'MWQ_Deliberate'].values
+Xz = zscore(X)
 
-# do pca
-pca = PCA(svd_solver='full').fit(clean_data)
+pca = PCA(svd_solver='full').fit(Xz)
 
-# varimax rotation
-vm = varimax(pca.components_.T * -1)
-# plot output
-plt.matshow(vm, cmap="RdBu")
-plt.yticks(ticks=range(13), labels=data.columns[4:])
-
-# calculate principle component scores
-scores = clean_data.dot(vm)
+# calculate principle component scores; use the top three for dev
+scores = Xz.dot(pca.components_[:3, :].T)
 for i in range(scores.shape[-1]):
     data[f"factor_{i + 1}"] = scores[:, i]
-# data.iloc[:, 4:17] = data.iloc[:, 4:17].apply(zscore)
 
-# do lmm on each pc that take experiment design into account
-data["groups"] = 0  # we have only one group
+#%% parallel mixed modelling
 
-# variance component to specify the design
-vcf = {"RIDNO": "0 + C(RIDNO)",  # participant ID
-       "nBack": "0 + C(nBack)",  # two expeirmental conditions: 0-back and 1-back
-       "session": "0 + C(session)"}  # time - not expecting much 
+# define model
+# this is the same as the following model in R
+# lmer(factor_i ~  1 + C(nBack) + (1|C(RIDNO)) + (1|C(session)), data=data) 
+# i = 1, ...., m 
+# m is the number of componensts
+# zero back conditon is the reference condition
+# intercept as fixed effect
+# variance component to specify the design - fit random intercept
+model = {
+    "formula": f"factor_{i + 1} ~ 1 + C(nBack)",  
+    "groups": "RIDNO",
+    "vcf": {"session": "0 + C(session)",  # time - not expecting much 
+           "RIDNO": "0 + C(RIDNO)"  # subject
+           }
+}
 
 model_parameters = []
 residual_matrix = []
 randome_effect = []
-for i in range(scores.shape[-1]):
-    # model formula, intercept as fixed effec
-    model_formula = f"factor_{i + 1} ~ 1"
-
+m_components = scores.shape[-1]
+h1_models = []
+for i in range(m_components):
+    print(f"{i + 1} / {m_components}")
+    mixed = smf.mixedlm(model["formula"], data, 
+                        groups=model["groups"], 
+                        vc_formula=model["vcf"])
     # fit the model
-    # this is the same as the following model in R
-    # lmer(factor_i ~ (1|RIDNO) + (1|nBack) + (1|session), data=data) 
-    # i = 1, ...., m 
-    # m is the number of componensts
-    mixed = smf.mixedlm(model_formula, data, 
-                        groups="groups", vc_formula=vcf)
     mixed_fit = mixed.fit()
-    # save model parameters
-    model_parameters.append(mixed_fit.params)
-    residual_matrix.append(mixed_fit.resid)
-    randome_effect.append(mixed_fit.random_effects)
-    # print(mixed_fit.summary())
 
-vc = {}
-for i, name in enumerate(mixed.exog_vc.names):
-    vc[name] = pd.DataFrame(mixed.exog_vc.mats[i][0], 
-                            columns=mixed.exog_vc.colnames[i][0])
+    # save fitted model
+    h1_models.append(mixed_fit)
 
+#%% effect matrix decomposition
 # rewrite the LMM as effect matrix decomposition
+
+# get the design matrix for the fixed effect (one vs zero back)
+var = data[["nBack"]]
+intercept = np.ones((data.shape[0], 1))
+var = np.hstack((intercept, var.values))
+
+# get the design matrix for the random effect
+re_names = mixed.exog_vc.names
+re_mats = mixed.exog_vc.mats
+
+# calculat the effect matrix
 Mf = []
 Mr = []
-for i in range(scores.shape[-1]):
-    mf = np.ones((data.shape[0], 1)).dot(model_parameters[i].Intercept)
-    Mf.append(np.squeeze(mf))
+residual_matrix = []
+for i in range(m_components):
+    model_parameters = h1_models[i].params
 
-    # get the estimate by variable just in case I need to break it down later
-    re = randome_effect[i][0]
-    mr = np.zeros((data.shape[0]))
-    for name in vc.keys():
-        idx_name = [f"{name}[{cn}]" for cn in vc[name].columns]
-        mat = vc[name]
-        tau_re = re[idx_name]
-        mr += np.dot(mat, tau_re)
-    Mr.append(mr)
+    # fixed effect
+    fe = model_parameters[fixed_var]
+    mf_j = []
+    for j, coef in enumerate(fe):
+        tau_fe = var[:, j]
+        mf_j.append(np.dot(coef, tau_fe))
+    Mf.append(np.array(mf_j))
 
-# quantify of effects importance with percent variance explained by each factor
+    # random effect
+    randome_effect = h1_models[i].random_effects
+    mr_i = []
+    for mats, name in zip(re_mats, re_names):
+        mr_k = []
+        for k, g in enumerate(randome_effect.keys()):
+            tau_idx = [idx for idx in randome_effect[g].index \
+                if name in idx]
+            tau = randome_effect[g][tau_idx].values
+            z = mats[k]
+            m = np.dot(z, tau)
+            mr_k += list(m)
+        mr_i.append(np.array(mr_k))
+    Mr_j.append(mr_i)
 
-# significance testing
+    # residual
+    residual_matrix.append(h1_models[i].resid)
 
-# visual representation of the effect matrices
+#%% quantify of effects importance with percent variance explained by each factor
+est_var_resid = np.var(np.array(residual_matrix), axis=1)  # redisuals
+
+est_var_random_j = []
+for mr_i in Mr_j:
+    est_var_random_j.append(np.var(mr_i, axis=1))
+
+est_var_fixed_j = []
+for mf_i in Mf_j:
+    est_var_fixed_j.append(np.var(mf_i, axis=1))
+
+est_var_full = np.sum(est_var_fixed_j, axis=1) \
+    + np.sum(est_var_random_j, axis=1) \
+    + est_var_resid
+
+# plot variace full model
+percent_var_exp = np.vstack((
+    np.array(est_var_fixed_j).T, 
+    np.array(est_var_random_j).T,
+    est_var_resid)) / est_var_full * 100
+percent_var_exp = pd.DataFrame(percent_var_exp,
+                               columns=np.arange(1, m_components + 1),
+                               index=fixed_var + list(vc.keys()) + ["residual"])
+percent_var_exp["Effect"] = percent_var_exp.index
+percent_var_exp = percent_var_exp.melt(id_vars="Effect", value_name="variance(%)", var_name="PC")
+
+# ignore intercept when plotting
+sns.barplot(x="Effect", y="variance(%)", hue="PC",
+            data=percent_var_exp[percent_var_exp["Effect"]!="Intercept"])
+plt.title("Variance of components")
+plt.show()
+#%% significance testing
+
+null_models = {
+    "nBack": {
+        "formula": f"factor_{i + 1} ~ 1"
+        "group": "RIDNO",
+        "vcf": {"session": "0 + C(session)", "RIDNO": "0 + C(RIDNO)"}
+        },
+    "RIDNO": {
+        "formula": f"factor_{i + 1} ~ 1 + C(nBack)"
+        "group": "group",
+        "vcf": {"session": "0 + C(session)"}
+        },
+    "session": {
+        "formula": f"factor_{i + 1} ~ 1 + C(nBack)"
+        "group": "RIDNO",
+        "vcf": {"RIDNO": "0 + C(RIDNO)"}
+        },
+}
+
+# run the null model
+
+
+#%% visual representation of the effect matrices
--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+import pandas as pd 
+import numpy as np 
+
+
 def varimax(Phi, gamma = 1, q = 20, tol = 1e-6):
     from numpy import eye, asarray, dot, sum, diag
     from numpy.linalg import svd
@@ -7,8 +11,22 @@ def varimax(Phi, gamma = 1, q = 20, tol = 1e-6):
     for i in range(q):
         d_old = d
         Lambda = dot(Phi, R)
-        u,s,vh = svd(dot(Phi.T,asarray(Lambda)**3 - (gamma/p) * dot(Lambda, diag(diag(dot(Lambda.T,Lambda))))))
+        u,s,vh = svd(dot(Phi.T,asarray(Lambda)**3 \
+            - (gamma/p) * dot(Lambda, diag(diag(dot(Lambda.T,Lambda))))))
         R = dot(u,vh)
         d = sum(s)
         if d/d_old < tol: break
     return dot(Phi, R)
+
+
+def correct_scale(data):
+    # correct each subject by used scale range
+    for id in np.unique(data.RIDNO):
+        id_idx = data['RIDNO'].str.match(id)
+        cur = data[id_idx].loc[:, "MWQ_Focus":"MWQ_Deliberate"].values
+        scling = np.max(cur.flatten())
+        floor = np.min(cur.flatten())
+        cur = (cur - floor) / scling
+        # update
+        data[id_idx].loc[:, "MWQ_Focus":"MWQ_Deliberate"] = cur
+    return data
